4c93ba1523c44e351a169db5b9710b5df4ffc68c
djangojazz <brettmorin2@gmail.com>
2015-09-06 21:12:44 -0700

    Added Cursor Adapter

--- app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
@@ -1,57 +1,34 @@
 package com.apress.gerber.reminders;
 
-import android.annotation.TargetApi;
-import android.database.Cursor;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class RemindersActivity extends ActionBarActivity {
-    private ListView mListView;
-    private RemindersDbAdapter mDbAdapter;
-    private RemindersSimpleCursorAdapter mCursorAdapter;
+public class RemindersActivity extends AppCompatActivity {
 
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private ListView mListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders);
         mListView = (ListView) findViewById(R.id.reminders_list_view);
-        mListView.setDivider(null);
-        mDbAdapter = new RemindersDbAdapter(this);
-        mDbAdapter.open();
-
-        Cursor cursor = mDbAdapter.fetchAllReminders();
-
-        //from columns defined in the db
-        String[] from = new String[]{
-                RemindersDbAdapter.COL_CONTENT};
-
-        //to the ids of views in the layout
-        int[] to = new int[]{
-                R.id.row_text
-        };
-
-        mCursorAdapter = new RemindersSimpleCursorAdapter(
+        //The arrayAdapter is the controller in our
+        //model-view-controllre relationship. (controller)
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
                 //context
-                RemindersActivity.this,
-                //the layout of the row
+                this,
+                //layout(view0
                 R.layout.reminders_row,
-                //cursor
-                cursor,
-                //from columns defined in the db
-                from,
-                //to the ids of views in the layout
-                to,
-                //flag - not used
-                0);
-        //the cursorAdapter (controller) is now updating the listView (view)
-        //with data from the db(model)
-        mListView.setAdapter(mCursorAdapter);
+                //row(view)
+                R.id.row_text,
+                //data(model) with bogus data to test our listview
+                new String[]{"first record", "second record", "third record"});
+        mListView.setAdapter(arrayAdapter);
     }
 
     @Override
--- app/src/main/java/com/apress/gerber/reminders/RemindersDbAdapter.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersDbAdapter.java
@@ -3,37 +3,33 @@ package com.apress.gerber.reminders;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 /**
- * Created by Brett on 8/31/2015.
+ * Created by ag on 5/1/2015.
  */
 public class RemindersDbAdapter {
+
     //these are the column names
-    public static final String COL_ID = "id";
+    public static final String COL_ID = "_id";
     public static final String COL_CONTENT = "content";
     public static final String COL_IMPORTANT = "important";
-
     //these are the corresponding indices
     public static final int INDEX_ID = 0;
     public static final int INDEX_CONTENT = INDEX_ID + 1;
     public static final int INDEX_IMPORTANT = INDEX_ID + 2;
-
-    // used for logging
+    //used for logging
     private static final String TAG = "RemindersDbAdapter";
-
     private DatabaseHelper mDbHelper;
     private SQLiteDatabase mDb;
-
     private static final String DATABASE_NAME = "dba_remdrs";
     private static final String TABLE_NAME = "tbl_remdrs";
     private static final int DATABASE_VERSION = 1;
-
     private final Context mCtx;
-
     //SQL statement used to create the database
     private static final String DATABASE_CREATE =
             "CREATE TABLE if not exists " + TABLE_NAME + " ( " +
