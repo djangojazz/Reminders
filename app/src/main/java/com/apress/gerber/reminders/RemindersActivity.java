eb1dbff4674d076f69d63ded1ef6f98473299f6c
djangojazz <brettmorin2@gmail.com>
2015-09-14 21:33:32 -0700

    Add Contextual mode

--- app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
@@ -2,13 +2,17 @@ package com.apress.gerber.reminders;
 
 import android.app.Dialog;
 import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
+import android.view.ActionMode;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -93,6 +97,51 @@ public class RemindersActivity extends ActionBarActivity {
                 });
             }
         });
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
+            mListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+            mListView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+                @Override
+                public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean
+                        checked) {}
+
+                @Override
+                public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                    MenuInflater inflater = mode.getMenuInflater();
+                    inflater.inflate(R.menu.cam_menu, menu);
+                    return true;
+                }
+
+                @Override
+                public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                    return false;
+                }
+
+                @Override
+                public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.menu_item_delete_reminder:
+                            for (int nC = mCursorAdapter.getCount() - 1; nC >= 0; nC--) {
+                                if (mListView.isItemChecked(nC)) {
+                                    mDbAdapter.deleteReminderById(getIdFromPosition(nC));
+                                }
+                            }
+                            mode.finish();
+                            mCursorAdapter.changeCursor(mDbAdapter.fetchAllReminders());
+                            return true;
+                    }
+                    return false;
+                }
+
+                @Override
+                public void onDestroyActionMode(ActionMode mode) {
+                }
+            });
+        }
+    }
+
+    private int getIdFromPosition(int nC) {
+        return (int)mCursorAdapter.getItemId(nC);
     }
 
     private void insertSomeReminders() {
